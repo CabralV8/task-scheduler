75bb2c2f0b2c0ac347b390bfd169d76b76184df5
Valberton <valberton77@gmail.com>
2025-07-12 22:59:17 -0300

    first commit

---
+++ src/test/java/com/proj/tasksscheduler/TasksSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proj.tasksscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
